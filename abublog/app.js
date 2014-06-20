eaa03da8f5595bbfbad8e59d5862dfe0b80baf7f
Mohamed Abushadi <mabushadi@gmail.com>
2014-06-20 00:57:28 -0400

    Style updates

--- abublog/app.js
+++ abublog/app.js
@@ -55,6 +55,6 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.listen(3000);
+//app.listen(3000);
 
 module.exports = app;
--- abublog/routes/index.js
+++ abublog/routes/index.js
@@ -3,16 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Blog' });
 });
 
-router.get('/article/add', function(req, res){
-	res.render('articleadd', {title:"Add article"});
-});
-
-router.get('/article/save', function(req, res){
-	res.write('saving');
-	res.end();
-});
 
 module.exports = router;
